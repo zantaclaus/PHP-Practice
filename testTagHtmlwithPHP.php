bc150105bf5b0fb87f78c9a7d9e617787b0c66cc
Tarzuto <tarzuto@gmail.com>
2021-01-02 21:49:08 +0700

    Use Html Tags

---
+++ testTagHtmlwithPHP.php
@@ -0,0 +1,10 @@
+<?php
+
+    echo '<h1>Hello World</h1>';
+    echo '<br>';
+
+    echo "<p> Test Tag P</p><br>";
+
+    // 'Red' -> must be single quote -> Do not same as <font></font> tags.
+    echo "<font color='Red'>Test color font</font>";
+?>

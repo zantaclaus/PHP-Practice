32d5fa4051cb67fa02351dd574ecdb42b411ba6f
Tarzuto <tarzuto@gmail.com>
2021-01-03 20:15:07 +0700

    Number Form

---
+++ testFormNumber.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Number Form</title>
+</head>
+<body>
+    <form action="" method="POST">
+        mynumber
+        <input type="number" name="mynamber" min="1" max="9" value="0">
+        <br>
+        <button type="submit">Send</button>
+    </form>
+</body>
+</html>

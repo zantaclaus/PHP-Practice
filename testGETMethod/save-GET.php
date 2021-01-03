36a5cc0a6d101cc1a0a4bb3a918010459a1659d7
Tarzuto <tarzuto@gmail.com>
2021-01-03 19:55:57 +0700

    GET Method

---
+++ testGETMethod/save-GET.php
@@ -0,0 +1,14 @@
+<?php
+    echo "<pre>";
+    print_r($_GET);
+    echo "</pre>";
+
+    echo "<hr>";
+    
+    echo "<pre>";
+    var_dump($_GET);
+    echo "</pre>";
+
+    echo "<hr>";
+    echo "searching for ".$_GET['search']; // search is name of <input></input>
+?>
---
+++ testGETMethod/testRegister-GET.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Get Method</title>
+</head>
+<body>
+    <form action="save-GET.php" method="get">
+        Search
+        <input type="text" name="search" required>
+        <br>
+
+        <button type="submit">Press</button>
+    </form>
+</body>
+</html>

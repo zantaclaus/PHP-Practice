8894337d3e8d09ea7ee40ae6efdeae6062848c50
Tarzuto <tarzuto@gmail.com>
2021-01-04 20:15:25 +0700

    Radio Form

---
+++ testFormRadio/testFormRadioBtn.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="save-Radio.php" method="post" >
+        <input type="radio" name="pay" value="1" checked required> เงินสด <br>
+        <input type="radio" name="pay" value="2" required> บัตรเครดิต <br>
+        <button type="submit">ชำระเงิน</button>
+    
+    </form>
+</body>
+</html>

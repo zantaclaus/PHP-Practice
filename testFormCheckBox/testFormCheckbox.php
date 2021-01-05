3deecf9eeb97933ca6b8f74ba1bf3b83950de4bf
Tarzuto <tarzuto@gmail.com>
2021-01-05 14:51:38 +0700

    CheckBox Form

---
+++ testFormCheckBox/testFormCheckbox.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Checkbox</title>
+</head>
+<body>
+    <form action="save-Checkbox.php" method="post">
+        <input type="checkbox" name="BMW" value="BMW"> BMW <br>
+        <input type="checkbox" name="McBenz" value="McBenz"> McBenz <br> 
+        <input type="checkbox" name="Toyota" value="Toyota"> Toyota <br>
+        <button type="submit">Submit</button> 
+    </form>
+</body>
+</html>

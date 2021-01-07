d6b4eb113097a25b1291d8a04074a4bbb81be3cf
Tarzuto <tarzuto@gmail.com>
2021-01-07 15:33:55 +0700

    Email Form

---
+++ testFormEmail/testFormEmail.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Email Form</title>
+</head>
+<body>
+    <form action="" method="post">
+        <input type="email" name="myemail" required placeholder="xxx@xxx.xxx">
+        <button type="submit">Submit</button>
+    </form>
+</body>
+</html>
